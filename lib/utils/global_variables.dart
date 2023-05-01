588857233b6bcc7955c5fd8e5140039e414f5689
suketupiprotat <suketupiprotar9930@gmail.com>
2023-05-01 16:18:32 +0530

    created screen for feed

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -5,7 +5,7 @@ import 'package:instagram_clone/screens/feed_screen.dart';
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Text("feed"),
+  FeedScreen(),
   Text('search'),
   AddPostScreen(),
   Text('notify'),
